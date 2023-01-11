6a425240ccfc93b26d87037a586720081540f29a
Notuc <98197211+Notuc@users.noreply.github.com>
2023-01-11 11:52:33 -0500

    Remove the code from the vue temeplate
    Add a header componenet

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,24 @@
 
 <template>
   <div class="container">
-    <h1>Hello World</h1>
+    <Header title="Task Tracker"/>
   </div>
 </template>
+
+<script>
+  import Header from './components/Header.vue'
+
+  export default {
+  name: 'app',
+  components: {
+    Header,
+  }
+  }
+</script>
+
+
+
+
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
