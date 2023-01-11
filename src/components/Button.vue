eb41829c05e69025c614e7051b83950e90a6a366
Notuc <98197211+Notuc@users.noreply.github.com>
2023-01-11 14:45:32 -0500

    Update Button.vue

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,12 +1,16 @@
 <template>
-    <button class="btn">
-        Add Task
+    <button :style="{background:color }" class="btn">
+        {{text}}
     </button>
 </template>
 
 <script>
     export default {
         name: 'Button',
+        props: {
+            text: String,
+            color: String
+        }
     }
 </script>
 
